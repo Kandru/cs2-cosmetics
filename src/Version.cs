d9c2d643f3b62e95457b5bb97bfd23a25541ab7d
Kalle <kalle@kandru.de>
2024-11-18 11:45:43 +0100

    remove unused code

--- src/Version.cs
+++ src/Version.cs
@@ -1,10 +1,9 @@
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Utils;
 
 namespace Cosmetics
 {
     public partial class Cosmetics : BasePlugin
     {
-        public override string ModuleVersion => "1.0.3";
+        public override string ModuleVersion => "1.0.4";
     }
 }
