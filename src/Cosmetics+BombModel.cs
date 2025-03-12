1d039b7cd78874f65b6fe9706adb532f9e2a4755
Kalle <info@kalleminkner.de>
2025-03-12 10:47:18 +0100

    improve bomb model sizing

--- src/Cosmetics+BombModel.cs
+++ src/Cosmetics+BombModel.cs
@@ -13,13 +13,17 @@ namespace Cosmetics
             if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower())
                 && !Config.MapConfigs[Server.MapName.ToLower()].BombModel.Enable) return;
             // register event handler
-            RegisterEventHandler<EventBombPlanted>(BombModelOnBombPlanted);
+            if (Config.MapConfigs[Server.MapName.ToLower()].BombModel.ModifyPlantedC4)
+                RegisterEventHandler<EventBombPlanted>(BombModelOnBombPlanted);
+            if (Config.MapConfigs[Server.MapName.ToLower()].BombModel.ModifyWeaponC4)
+                RegisterEventHandler<EventBombPickup>(BombModelOnBombPickup);
         }
 
         private void ResetBombModel()
         {
             // unregister event handler
             DeregisterEventHandler<EventBombPlanted>(BombModelOnBombPlanted);
+            DeregisterEventHandler<EventBombPickup>(BombModelOnBombPickup);
         }
 
         private HookResult BombModelOnBombPlanted(EventBombPlanted @event, GameEventInfo info)
@@ -27,19 +31,39 @@ namespace Cosmetics
             // delay one frame to allow c4 model to exist
             Server.NextFrame(() =>
             {
+                // check for map specific configuration
+                BombModelConfig? mapConfig = null;
+                if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower()))
+                    mapConfig = Config.MapConfigs[Server.MapName.ToLower()].BombModel;
+                else
+                    mapConfig = Config.Global.BombModel;
+                // find all planted c4 entities
                 var bombEntities = Utilities.FindAllEntitiesByDesignerName<CPlantedC4>("planted_c4");
                 foreach (var bombEntity in bombEntities)
                 {
-                    float randomScale = 3.0f;
-                    if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower()))
-                    {
-                        var mapConfig = Config.MapConfigs[Server.MapName.ToLower()];
-                        randomScale = (float)(new Random().NextDouble() * (mapConfig.BombModel.MaxSize - mapConfig.BombModel.MinSize) + mapConfig.BombModel.MinSize);
-                    }
-                    else
-                    {
-                        randomScale = (float)(new Random().NextDouble() * (Config.Global.BombModel.MaxSize - Config.Global.BombModel.MinSize) + Config.Global.BombModel.MinSize);
-                    }
+                    float randomScale = (float)(new Random().NextDouble() * (mapConfig.MaxSize - mapConfig.MinSize) + mapConfig.MinSize);
+                    bombEntity.CBodyComponent!.SceneNode!.GetSkeletonInstance().Scale = randomScale;
+                }
+            });
+            return HookResult.Continue;
+        }
+
+        private HookResult BombModelOnBombPickup(EventBombPickup @event, GameEventInfo info)
+        {
+            // delay one frame to allow c4 model to exist
+            Server.NextFrame(() =>
+            {
+                // check for map specific configuration
+                BombModelConfig? mapConfig = null;
+                if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower()))
+                    mapConfig = Config.MapConfigs[Server.MapName.ToLower()].BombModel;
+                else
+                    mapConfig = Config.Global.BombModel;
+                // find all planted c4 entities
+                var bombEntities = Utilities.FindAllEntitiesByDesignerName<CPlantedC4>("weapon_c4");
+                foreach (var bombEntity in bombEntities)
+                {
+                    float randomScale = (float)(new Random().NextDouble() * (mapConfig.MaxSize - mapConfig.MinSize) + mapConfig.MinSize);
                     bombEntity.CBodyComponent!.SceneNode!.GetSkeletonInstance().Scale = randomScale;
                 }
             });
--- src/Cosmetics+Config.cs
+++ src/Cosmetics+Config.cs
@@ -6,6 +6,8 @@ namespace Cosmetics
     public class BombModelConfig
     {
         [JsonPropertyName("enable")] public bool Enable { get; set; } = true;
+        [JsonPropertyName("modify_planted_c4")] public bool ModifyPlantedC4 { get; set; } = true;
+        [JsonPropertyName("modify_weapon_c4")] public bool ModifyWeaponC4 { get; set; } = true;
         [JsonPropertyName("min_size")] public float MinSize { get; set; } = 3f;
         [JsonPropertyName("max_size")] public float MaxSize { get; set; } = 7f;
     }
