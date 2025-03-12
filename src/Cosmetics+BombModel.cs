0dbaba0cb09f5c4d8490c5ae4df21057292e2303
Kalle <kalle@kandru.de>
2025-03-12 12:28:48 +0100

    fix: bomb model size

--- src/Cosmetics+BombModel.cs
+++ src/Cosmetics+BombModel.cs
@@ -5,17 +5,22 @@ namespace Cosmetics
 {
     public partial class Cosmetics : BasePlugin
     {
+        private BombModelConfig? bombModelConfig = null;
         private void InitializeBombModel()
         {
             // disable if globally disabled
             if (!Config.Global.BombModel.Enable) return;
+            // set map specific configuration
+            if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower()))
+                bombModelConfig = Config.MapConfigs[Server.MapName.ToLower()].BombModel;
             // disable if map specific disabled
-            if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower())
-                && !Config.MapConfigs[Server.MapName.ToLower()].BombModel.Enable) return;
+            if (bombModelConfig != null && !bombModelConfig.Enable) return;
             // register event handler
-            if (Config.MapConfigs[Server.MapName.ToLower()].BombModel.ModifyPlantedC4)
+            if (bombModelConfig != null && bombModelConfig.ModifyPlantedC4
+                || bombModelConfig == null && Config.Global.BombModel.ModifyPlantedC4)
                 RegisterEventHandler<EventBombPlanted>(BombModelOnBombPlanted);
-            if (Config.MapConfigs[Server.MapName.ToLower()].BombModel.ModifyWeaponC4)
+            if (bombModelConfig != null && bombModelConfig.ModifyWeaponC4
+                || bombModelConfig == null && Config.Global.BombModel.ModifyWeaponC4)
                 RegisterEventHandler<EventBombPickup>(BombModelOnBombPickup);
         }
 
@@ -53,17 +58,15 @@ namespace Cosmetics
             // delay one frame to allow c4 model to exist
             Server.NextFrame(() =>
             {
-                // check for map specific configuration
-                BombModelConfig? mapConfig = null;
-                if (Config.MapConfigs.ContainsKey(Server.MapName.ToLower()))
-                    mapConfig = Config.MapConfigs[Server.MapName.ToLower()].BombModel;
-                else
-                    mapConfig = Config.Global.BombModel;
                 // find all planted c4 entities
-                var bombEntities = Utilities.FindAllEntitiesByDesignerName<CPlantedC4>("weapon_c4");
+                var bombEntities = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("weapon_c4");
                 foreach (var bombEntity in bombEntities)
                 {
-                    float randomScale = (float)(new Random().NextDouble() * (mapConfig.MaxSize - mapConfig.MinSize) + mapConfig.MinSize);
+                    float randomScale = 1f;
+                    if (bombModelConfig != null)
+                        randomScale = (float)(new Random().NextDouble() * (bombModelConfig.MaxSize - bombModelConfig.MinSize) + bombModelConfig.MinSize);
+                    else
+                        randomScale = (float)(new Random().NextDouble() * (Config.Global.BombModel.MaxSize - Config.Global.BombModel.MinSize) + Config.Global.BombModel.MinSize);
                     bombEntity.CBodyComponent!.SceneNode!.GetSkeletonInstance().Scale = randomScale;
                 }
             });
