c1bbb21156efb97588bfb4cc39140dd0a93ef1e4
Kalle <kalle@kandru.de>
2025-07-06 17:25:57 +0200

    bump version to 1.1.17

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace Cosmetics
 {
     public partial class Cosmetics : BasePlugin
     {
-        public override string ModuleVersion => "1.1.16";
+        public override string ModuleVersion => "1.1.17";
     }
 }
