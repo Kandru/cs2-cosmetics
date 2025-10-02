5c8320da2255ded68a57311c17fafaa6fe744fef
derkalle4 <kalle@kandru.de>
2025-10-02 17:39:54 +0200

    bump version to 25.10.1

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace Cosmetics
 {
     public partial class Cosmetics : BasePlugin
     {
-        public override string ModuleVersion => "1.2.3";
+        public override string ModuleVersion => "25.10.1";
     }
 }
